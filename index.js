23c9a928f58a7816fe33c47451ac5edd80185d35
Daniel Luxemburg <daniel.luxemburg@gmail.com>
2016-10-01 20:06:46 -0400

    2.0.0 initial commit

--- index.js
+++ index.js
@@ -1,48 +1,13 @@
-var express = require('express'),
-    Leap = require('leapjs'),
-    app = express()
-    server = require('http').createServer(app)
-    io = require('socket.io').listen(server)
-    leapLifx = require('./lib/leap-lifx').createLeapLifx({
-      controller: new Leap.Controller()
-    })
+const LeapLifx = require('./lib/leap-lifx');
+const LeapSource = require('./lib/leap-source');
 
-var leftLifx = require('./lib/lifx').createLifx({
-  minLights: 1,
-  defaultLabel: 'Left'
-})
+leapSource = new LeapSource();
+leapLifx = new LeapLifx();
 
-var rightLifx = require('./lib/lifx').createLifx({
-  minLights: 1,
-  defaultLabel: 'Right'
-})
+leapSource.on('frame', frame => {
+  let arr = frame.hands[0];
+  if (!arr) return;
+  leapLifx.hsb(arr[0] * 360, arr[2] * 100, arr[1] * 100);
+});
 
-var lifxs = [leftLifx, rightLifx]
-
-app.use(express.static(__dirname + '/static'));
-app.set('port', process.env.PORT || '8888')
-io.set('log level', 2)
-
-leapLifx.on('frame', function(frame){
-
-  try {
-    io.sockets.emit('frame', frame)
-  } catch(e) {
-    console.log('Failed to serialize frame: '+e.message)
-  }
-
-  frame.hands.forEach(function(hand, index){
-    if(lifxs[index]) lifxs[index].hsl(hand.hsl)
-  })
-
-})
-
-server.listen(app.settings.port, function(){
-  console.log('App server listening at http://localhost:'+app.settings.port)
-})
-
-leftLifx.start()
-rightLifx.start()
-leapLifx.start()
-
-
+leapSource.run();
